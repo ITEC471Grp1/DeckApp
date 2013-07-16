1ba154f604ffbfd5805c47e6079f7f6e2b6e5459
bmiller7 <bmiller7@email.radford.edu>
2013-07-15 20:00:51 -0400

    Update CalulatorMain.java

--- DeckApp/src/com/itec471/deckcalculator/CalulatorMain.java
+++ DeckApp/src/com/itec471/deckcalculator/CalulatorMain.java
@@ -2,6 +2,7 @@ package com.itec471.deckcalculator;
 //Chris GitHub test 7/12
 //Matt H Test 7/15/2013
 // Jamie GitHub test 7/13
+//Brandon test 7/15
 
 import java.util.ArrayList;
 import java.util.List;
